57a71769afdd842c2637913670c2876dc5b50236
LAVIDANGE DONNE Jimmy <jldonne@gmail.com>
2020-08-30 23:39:20 +0200

    update user relation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,9 +41,15 @@ class User implements UserInterface
      */
     private $tricks;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="user", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->tricks = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -149,6 +155,37 @@ class User implements UserInterface
             // set the owning side to null (unless already changed)
             if ($trick->getUsers() === $this) {
                 $trick->setUsers(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getUser() === $this) {
+                $comment->setUser(null);
             }
         }
 
